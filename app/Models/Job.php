f0f275842bc3860a76b3a9a2c46d668996ed7d87
EtimbukUdofia <etimbukudofia9@gmail.com>
2024-09-09 16:14:47 +0100

    update job model

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Job extends Model  // We extend model to create an Eloqent model
 {
+  use HasFactory;
+
   protected $table = "job_listings";
 
   protected $fillable = ['title', 'salary'];
