0ae3b652cb2b5f1f25797d66c637e10a60366dd9
EtimbukUdofia <etimbukudofia9@gmail.com>
2024-09-16 10:39:23 +0100

    update show view add link to edit a job

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -5,8 +5,14 @@
 
     <a href="/jobs" class="text-blue-500">Go Back</a>
     
-    <h2 class="font-bold text-lg">{{$job['title']}}</h2>
+    <h2 class="font-bold text-lg">{{$job->title}}</h2>
     <p>
-        This job pays <strong>{{$job['salary']}}</{strong}> per year.
+        This job pays <strong>{{$job->salary}}</{strong}> per year.
+    </p>
+
+    <p class="mt-6">
+        <x-button href="/jobs/{{$job->id}}/edit">
+            Edit Job
+        </x-button>
     </p>
 </x-layout>
